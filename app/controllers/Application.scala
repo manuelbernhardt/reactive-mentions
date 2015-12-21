860cb7a4d111ba037d458262c7813e30cb330128
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2015-12-21 10:38:38 +0100

    Displaying the mentions count to the user

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
+import java.sql.Timestamp
+import javax.inject.Inject
+
+import database.DB
+import org.joda.time.DateTime
 import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject() (db: DB) extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action.async { implicit request =>
+
+    import generated.Tables._
+    import org.jooq.impl.DSL._
+
+    db.query { sql =>
+      val mentionsCount = sql.select(
+        count()
+      ).from(MENTIONS)
+       .where(
+         MENTIONS.CREATED_ON.gt(value(new Timestamp(DateTime.now.minusDays(1).getMillis)))
+       ).execute()
+
+      Ok(views.html.index(mentionsCount))
+    }
+
   }
 
 }
